920db690542a10826d9c9b440df0553e58d1b719
Ahmed Ali <Ahmed97028@gmail.com>
2026-07-26 04:12:50 +0400

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,46 @@
+import type { NextConfig } from "next";
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com https://cdn.jsdelivr.net;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://*.supabase.co;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  connect-src 'self' https://*.supabase.co wss://*.supabase.co https://tesseract.projectnaptha.com https://unpkg.com https://cdn.jsdelivr.net;
+  worker-src 'self' blob:;
+  upgrade-insecure-requests;
+`
+
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
+};
+
+export default nextConfig;
